63f29f48dae8d06618240dd49f81e99fd2bd6dec
Jordanbouaissa25 <jordanbouaissa25@mail.com>
2024-04-29 16:30:07 +0200

    Find

--- events.js
+++ events.js
@@ -1,15 +1,36 @@
-console.log("Bienvenue");
-var calcul = (12 / 2) * 4;
+// const { reject } = require("lodash");
 
-console.log(calcul);
-setTimeout(function () {
-  try {
-    calcul = 4;
-    console.log(calcul);
-    ds;
-  } catch (err) {
-    console.log(err.name);
-  } finally {
-    console.warn("FIN");
-  }
-}, 1000);
+// console.log("Bienvenue");
+// var calcul = (12 / 2) * 4;
+
+// console.log(calcul);
+// setTimeout(function () {
+//   try {
+//     calcul = 4;
+//     console.log(calcul);
+//     ds;
+//   } catch (err) {
+//     console.log(err.name);
+//   } finally {
+//     console.warn("FIN");
+//   }
+// }, 1000);
+
+function waiting(time) {
+  return new Promise((resolve, reject) => {
+    if (time > 100)
+      setTimeout(() => {
+        console.log("OK");
+        resolve({ succes: true });
+      }, time);
+    else reject({ succes: false });
+  });
+}
+
+waiting(101)
+  .then((value) => {
+    console.log("Promise reussi :", value);
+  })
+  .catch((err) => {
+    console.log("Promise échoue :", err);
+  });
---
+++ findtab_promise.js
@@ -0,0 +1,63 @@
+var prompt = require("prompt");
+
+var people = [
+  {
+    // Recursivité en Javascript "parent-enfant"
+    firstName: "Edouard",
+    lastName: "Bernier",
+    children: [
+      {
+        firstName: "Jean",
+        lastName: "Bernier",
+        children: [
+          {
+            firstName: "Junior",
+            lastName: "Bernier",
+            children: [
+              {
+                firstName: "Jordan",
+                lastName: "Bernier",
+              },
+              {
+                firstName: "Luc",
+                lastName: "Bernier",
+              },
+            ],
+          },
+        ],
+      },
+      {
+        firstName: "Sacha",
+        lastName: "Bernier",
+      },
+      {
+        firstName: "Laurent",
+        lastName: "Bernier",
+      },
+      {
+        firstName: "Raphael",
+        lastName: "Bernier",
+      },
+    ],
+  },
+  {
+    firstName: "Jean",
+    lastName: "Luc",
+    children: [
+      {
+        firstName: "Richard",
+        lastName: "Luc",
+        children: [
+          {
+            firstName: "Roméo",
+            lastName: "Luc",
+          },
+        ],
+      },
+    ],
+  },
+];
+
+// Créer un tableau avec tout les mêmes nom de famille , si on tappe Luc on trouvera tout les luc ainsi que tout les édouards
+// Ajouter dans un tableau et retourné.
+// Retourner les autres personnes par apport à l'ancien exercice.
